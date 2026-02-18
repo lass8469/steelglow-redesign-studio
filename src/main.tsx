679759e4c778cdca1ca1c34d9ec658b671681a86
lass8469 <smite1516@gmail.com>
2026-02-18 12:48:44 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { ViteReactSSG } from 'vite-react-ssg'
+import App from './App'
+import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+// This replaces the standard ReactDOM.createRoot
+export const createApp = ViteReactSSG(
+  <App />
+)
